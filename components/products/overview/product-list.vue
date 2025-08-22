35187e3b32112a3dbb88af8e00419a09f6697633
MinhQuan1912 <nguyenquan19122002@gmail.com>
2025-08-22 10:38:21 +0700

    create product table

--- components/products/overview/product-list.vue
+++ components/products/overview/product-list.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="product-list">
+        <!-- Header -->
         <div class="flex p-3 justify-between items-center">
             <div class="flex pl-5 items-center justify-between gap-6">
                 <h6 class="text-primary text-xl leading-[145%] font-semibold">Products</h6>
@@ -20,7 +21,24 @@
                 </div>
             </div>
         </div>
+        <!-- Table -->
+        <div class="px-4">
+            <div class="flex gap-6 px-4 justify-between items-center">
+                <div class="w-128 h-6 flex gap-5 text-tertiary">
+                    <div class="w-6 h-6 border-2 border-stroke rounded-xs"></div>
+                    <div class="text-xs leading-[160%] opacity-80">Product</div>
+                </div>
+                <div class="flex py-2 justify-evenly items-center text-xs text-tertiary leading-[160%] opacity-80 flex-1">
+                    <div class="">Status</div>
+                    <div class="">Price</div>
+                    <div class="">Sales</div>
+                    <div>Views</div>
+                    <div>Likes</div>
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
 <script setup>
