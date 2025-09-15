6b137bd6a9e9861aeba32d28f53bff4b3eae5a38
MinhQuan1912 <nguyenquan19122002@gmail.com>
2025-09-15 16:40:48 +0700

    fix modal

--- pages/Product/released.vue
+++ pages/Product/released.vue
@@ -145,7 +145,7 @@
         <div class="flex flex-col gap-4 w-full sm:w-[calc(50%-8px)] ">
           <div class="flex flex-col gap-1 xs:gap-2 w-full">
             <div class="text-base xl:text-lg">Product name</div>
-            <input type="text" class="!border border-black rounded-xl p-2 text-sm xs:text-xl h-8 xs:h-12"
+            <input type="text" class="!border-[1.5px] border-black rounded-xl p-2 text-sm xs:text-xl h-8 xs:h-12"
               v-model="editProduct.name">
           </div>
           <div class="flex flex-col gap-1 xs:gap-2 w-full">
@@ -156,7 +156,7 @@
           </div>
           <div class="flex flex-col gap-1 xs:gap-2 w-full">
             <div class="text-base xl:text-lg">Product price</div>
-            <input type="number" class="!border border-black rounded-xl p-2 text-sm xs:text-xl h-8 xs:h-12"
+            <input type="number" class="!border-[1.5px] border-black rounded-xl p-2 text-sm xs:text-xl h-8 xs:h-12"
               v-model="editProduct.price">
           </div>
         </div>
